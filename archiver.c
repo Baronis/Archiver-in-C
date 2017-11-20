a2dc2b176827fa52275a6c5ee67236855ff8c5a8
Matheus Baroni <matheuspbaroni@gmail.com>
2017-11-20 18:16:13 -0200

    Funções e interface implementados.
    Issue: erro ao extrair após uma exclusão.

--- archiver.c
+++ archiver.c
@@ -1,3 +1,5 @@
+// gcc archiver.c -o archiver -lm -std=c99
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,56 +7,48 @@
 #include <math.h>
 
 /**
-* Simple Archiver
+* Gerenciador de Archives
 * @author Edson da Costa Vitor Junior
 * @author Guilherme Atihe de Oliveira
 * @author Gustavo Batistic Ribeiro
-* @author Matheus Baroni
+* @author Matheus Peron Baroni
 *
-*
-* Archiver Header Structure
-* Date yy-mm-dd-hh-mm-ss
-* Number of files included
-* Next File Pointer (RRN)
 * 
-* File Header
-* Next File Pointer (RRN)
-* Name Size
-* Name
-* Is Deleted?
-* File Size /512 rounded up (2 bytes)
+* Estrutura de cabecalho
+    Date yy-mm-dd-hh-mm-ss
+    Number of files included
+    File table
+      id 1 byte
+      is deleted 1 byte
+      file length 2 bytes (/512 - ceil)
+* 
+* Cabecalho individual
+    Tamnaho do nome
+    Nome
 *
 *
-* ! Importante
-* Converter numero de 2 bytes para 2 bytes independentes
-*
-* unsigned short number = ceil(ftell(file)/512);
-  unsigned char p1;
-  p1 = *((unsigned char*)(&number));
-  fputc(p1, archive);
-  unsigned char p2;
-  p2 = *((unsigned char*)(&number) + 1);
-  fputc(p2, archive);
-*
-* Contrario
-*
-* unsigned short x = p1 | p2 << 8;
 */
 
-int delete(char *archiveName, char *fileName) {
+int extract(char *archiveName, char *fileName) {
   FILE *archive, *file;
 
   char *archiveFileName = malloc(strlen(archiveName)+strlen(".arc")+1);
   strcpy(archiveFileName, archiveName);
   strcat(archiveFileName, ".arc");
 
-  int a;
+  int iT, iF = 512, length;
+  unsigned short bb;
+  //unsigned short s = 0; 
+  unsigned char b1, b2;
+  int a, x, count = 0;
+  char *n;
 
-  file = fopen(fileName, "w+");
+  file = fopen(fileName, "wb+");
   if (file == NULL) {
     puts("Impossivel abrir arquivo.");
     exit(1);
   }
+  printf("Novo arquivo gerado.\n");
 
   archive = fopen(archiveFileName, "rb+");
   if (archive == NULL) {
@@ -62,20 +56,38 @@ int delete(char *archiveName, char *fileName) {
     fclose(file);
     exit(1);
   }
-  int index = 7;
-  while(1) {
-    a = fgetc(archive);
-    if(!a)
-      break;
-    else {
-      fseek(archive, (a*512), SEEK_SET);
-    }
-  }
+
+  fseek(archive, 6, SEEK_SET);
+
   a = fgetc(archive);
-  fseek(archive, a, SEEK_CUR);
-  fputc(1, archive);
-  while ( (a = fgetc(file)) != EOF )
-    fputc(a, archive);
+  if(a == 0) 
+    return 0;
+  a = fgetc(archive);
+  while (a > 0) {
+    x = fgetc(archive);
+    b1 = fgetc(archive);
+    b2 = fgetc(archive);
+    bb = b1 | b2 << 8;
+    iT = ftell(archive);
+
+    fseek(archive, iF, SEEK_SET);
+    a = fgetc(archive);
+    n = malloc(sizeof(char)*a);
+    for (int i = 0; i < a; ++i)
+       n[i] = fgetc(archive);
+    if (strcmp(n,fileName) == 0) {
+      length = bb*512;
+      for (int i = 0; i < length; ++i) {
+        a = fgetc(archive);
+        fputc(a, file);
+      }
+      break;
+    }
+    fseek(archive, iT, SEEK_SET);
+    a = fgetc(archive);
+    iF += bb*512;
+  }
+
   fclose(file);
   fclose(archive);
 
@@ -87,8 +99,11 @@ int list(char *name) {
   char *fileName = malloc(strlen(name)+strlen(".arc")+1);
   strcpy(fileName, name);
   strcat(fileName, ".arc");
-
-  int a, x;
+  int iT, iF = 512;
+  unsigned short bb;
+  unsigned short s = 0; 
+  unsigned char b1, b2;
+  int a, x, count = 0;
 
   archive = fopen(fileName, "rb+");
   if (archive == NULL) {
@@ -101,32 +116,33 @@ int list(char *name) {
   fseek(archive, 6, SEEK_CUR);
   a = fgetc(archive);
   printf("Numero de arquivos: %d\n", a);
-
-  x = fgetc(archive);
-  if(a == 0)
+  if(a == 0) 
     return 0;
+  a = fgetc(archive);
+  while (a > 0) {
+    
+    x = fgetc(archive);
+    if(x == 0) {
+      printf("Arquivo %d: ", a);
+      b1 = fgetc(archive);
+      b2 = fgetc(archive);
+      bb = b1 | b2 << 8;
+      iT = ftell(archive);
 
-  if (a == 1) {
-    fseek(archive, 513, SEEK_SET);
-    a = fgetc(archive);
-    printf("Arquivo 1: ");
-    for (int i = 0; i < a; ++i) {
-      printf("%c", fgetc(archive));
-    }
-    printf("\nFim da Lista!\n");
-  } else {
-    fseek(archive, 0, SEEK_SET);
-    fseek(archive, 513, SEEK_CUR);
-    while(a > 0) {
+      fseek(archive, iF, SEEK_SET);
       a = fgetc(archive);
-      printf("Arquivo id %d: ", x);
-      for (int i = 0; i < a; ++i) {
+      for (int i = 0; i < a; ++i)
         printf("%c", fgetc(archive));
-      }
-      fseek(archive, ((a+2)*-1), SEEK_CUR);
-      fseek(archive, a*512, SEEK_CUR);
+      printf("\n");
+      fseek(archive, iT, SEEK_SET);
+      a = fgetc(archive);
+      iF += bb*512;
+    } else {
+      a = fgetc(archive);
+      a = fgetc(archive);
+      a = fgetc(archive);
     }
-    printf("\nFim da Lista!\n");
+    
   }
 
   fclose(archive);
@@ -134,11 +150,96 @@ int list(char *name) {
   return 0;
 }
 
-int subtract(char *name) {
+int subtract(char *archiveName, char *fileName) {
+  FILE *archive, *tmp;
+
+  char *archiveFileName = malloc(strlen(archiveName)+strlen(".arc")+1);
+  strcpy(archiveFileName, archiveName);
+  strcat(archiveFileName, ".arc");
+
+  int iT, iF = 512, iX, iM, length;
+  unsigned short bb;
+  unsigned short s = 0; 
+  unsigned char b1, b2;
+  int a, x, count = 0;
+  char *n;
+
+  int o;
+
+  archive = fopen(archiveFileName, "rb+");
+  if (archive == NULL) {
+    puts("Impossivel abrir archiver.");
+    fclose(archive);
+    exit(1);
+  }
+
+  tmp = fopen("temp.bin", "wb+");
+  if (archive == NULL) {
+    puts("Impossivel abrir arquivo temporario.");
+    fclose(tmp);
+    exit(1);
+  }
+
+  fseek(archive, 6, SEEK_SET);
+
+  a = fgetc(archive);
+
+  if(a == 0) 
+    return 0;
+
+  a = fgetc(archive);
+  while (a > 0) {
+    x = fgetc(archive); //Tratar caso de itens ja excluidos
+
+    b1 = fgetc(archive);
+    b2 = fgetc(archive);
+    bb = b1 | b2 << 8;
+
+    iT = ftell(archive); // Logo antes do proximo item da tabela
+
+    fseek(archive, iF, SEEK_SET); // Pulou pro 1o arq.
+
+    // Leitura do nome do primeiro arquivo
+    a = fgetc(archive);
+    n = malloc(sizeof(char)*a);
+    for (int i = 0; i < a; ++i)
+      n[i] = fgetc(archive);
+
+    if (strcmp(n, fileName) == 0) {
+      iX = iF;
+      iF += bb*512; // Comeco do proximo arq.
+      length = bb*512;
+
+      fseek(archive, (iT-3), SEEK_SET);
+      fputc(1, archive);
+
+      fseek(archive, 0, SEEK_END);
+      iM = ftell(archive);
+      fseek(archive, 0, SEEK_SET);
+
+      for (int i = 0; i < iM; ++i) {
+        
+        a = fgetc(archive);
+        x = ftell(archive);
+        if( (x > iX) && (x <= (iX+length)) ){
+          continue;
+        }
+        fputc(a, tmp);
+      }
+      break;
+    } 
+    fseek(archive, iT, SEEK_SET);
+    a = fgetc(archive);
+    iF += bb*512;
+  }
+  fclose(archive);
+  fclose(tmp);
+
+  remove(archiveFileName);
+  rename("temp.bin", archiveFileName);
+
   return 0;
 }
-
-
 
 // Adiciona arquivo para o archiver
 int add(char *archiveName, char *fileName) {
@@ -148,10 +249,10 @@ int add(char *archiveName, char *fileName) {
   strcpy(archiveFileName, archiveName);
   strcat(archiveFileName, ".arc");
 
-  int a;
-  int index = 0;
-  unsigned short x; 
-  unsigned char p1, p2;
+  int a, count = 0;
+  unsigned short bb;
+  unsigned short s = 0; 
+  unsigned char b1, b2;
 
   file = fopen(fileName, "rb");
   if (file == NULL) {
@@ -160,7 +261,9 @@ int add(char *archiveName, char *fileName) {
   }
 
   fseek(file, 0L, SEEK_END);
-  unsigned short number = ceil(ftell(file)/512);
+  double sum = (ftell(file)+1+strlen(fileName));
+  unsigned short fileSize = (unsigned short) ceil(sum/512);
+  printf("-- %hu\n", fileSize);
   fclose(file);
 
   file = fopen(fileName, "rb");
@@ -184,49 +287,50 @@ int add(char *archiveName, char *fileName) {
 
   if(a == 0) {
     fseek(archive, -1, SEEK_CUR);
-    fputc(1, archive);
-    fseek(archive, 512, SEEK_SET);
+    count = 1;
+    fputc(count, archive);
+    count = 0;
+    fputc(count, archive);
+
+    b1 = *((unsigned char*)(&fileSize));
+    fputc(b1, archive);
+    b2 = *((unsigned char*)(&fileSize) + 1);
+    fputc(b2, archive);
+
     fputc(0, archive);
-    fputc(strlen(fileName), archive);
-    for (int i = 0; i < strlen(fileName); ++i) {
-      fputc(fileName[i], archive);
-    }
-    fputc(0, archive);
+
+    fseek(archive, 0, SEEK_SET);
+    fseek(archive, 512, SEEK_CUR);
+
   } else {
-    //Caso este arquivo nao seja o primeiro da lista
-    // Procurar o fim da lista
-    while(a > 0) {
-      fseek(archive, -1, SEEK_CUR);
-      for (int i = 0; i < a; ++i) {
-        fseek(archive, 512, SEEK_CUR);
-      }
+    count = 1;
+    do {
+      count++;
+      fseek(archive, 1, SEEK_CUR);
+      b1 = fgetc(archive);
+      b2 = fgetc(archive);
+      s += b1 | b2 << 8;
       a = fgetc(archive);
-    }
-    
-    // Chegou no ultimo!
-
-    a = fgetc(archive);
-    fseek(archive, (a+1), SEEK_CUR);
-    p1 = fgetc(archive);
-    p2 = fgetc(archive);
-    x = p1 | p2 << 8;
-    fseek(archive, (x*512), SEEK_CUR);
-
-    fputc(0, archive);
-    fputc(strlen(fileName), archive);
-    for (int i = 0; i < strlen(fileName); ++i) {
-      fputc(fileName[i], archive);
-    }
-    fputc(0, archive);
+      printf("%d\n", s);
+    } while(a > 0);
+    fseek(archive, -1, SEEK_CUR);
+    fputc(count, archive);
+    count = 0;
+    fputc(count, archive);
+    b1 = *((unsigned char*)(&fileSize));
+    fputc(b1, archive);
+    b2 = *((unsigned char*)(&fileSize) + 1);
+    fputc(b2, archive);
+    fseek(archive, 0, SEEK_SET);
+    fseek(archive, 512, SEEK_CUR);
+    fseek(archive, s*512, SEEK_CUR);
+    s = 0;
   }
 
-  // Armazenar tamanho do arquivo em dois bytes
-  
-  p1 = *((unsigned char*)(&number));
-  fputc(p1, archive);
+  fputc(strlen(fileName), archive);
 
-  p2 = *((unsigned char*)(&number) + 1);
-  fputc(p2, archive);
+  for (int i = 0; i < strlen(fileName); ++i)
+    fputc(fileName[i], archive);
 
   // Copiar arquivo
   while ( (a = fgetc(file)) != EOF )
@@ -249,8 +353,6 @@ int create(char *name) {
   time_t t = time(NULL);
   struct tm tm = *localtime(&t);
 
-  //Set Time in header
-  
   fputc(tm.tm_year, file);
   fputc(tm.tm_mon + 1, file);
   fputc(tm.tm_mday, file);
@@ -258,7 +360,6 @@ int create(char *name) {
   fputc(tm.tm_min, file);
   fputc(tm.tm_sec, file);
 
-  //set number of files included
   fputc(0, file);
   fputc(0, file);
 
@@ -266,16 +367,53 @@ int create(char *name) {
   return 1;
 }
 
-int main(){
-  printf("\nSimple C Archiver\nDesigned for ST562\n\nCreating file 'teste.arc'...\n");
+void help() {
+  printf("\e[1mGerenciador de Archives\nFeito para a disciplina ST562\e[0m\n");
+  printf("\n\e[1mCriacao de archive:\e[0m\n");
+  printf("\e[3m./archiver archive-name -c\e[0m\n\tCria novo archive.\n");
+  printf("\e[3m./archiver archive-name -c file.ext\e[0m\n\tCria novo archive e adiciona um arquivo.\n");
+  printf("\e[3m./archiver archive-name -c file1.ext file2.ext...\e[0m\n\tCria novo archive e adiciona varios arquivos dentro.\n");
+  printf("\n\e[1mAdicao de arquivos:\e[0m\n");
+  printf("\e[3m./archiver archive-name -a file.ext\e[0m\n\tAdiciona um arquivo ao archive.\n");
+  printf("\e[3m./archiver archive-name -a file1.ext file2.ext...\e[0m\n\tAdiciona varios arquivos ao archive.\n");
+  printf("\n\e[1mListagem de arquivos:\e[0m\n");
+  printf("\e[3m./archiver archive-name -l\e[0m\n\tLista os arquivos presentes no archive.\n");
+  printf("\n\e[1mExtracao de arquivos:\e[0m\n");
+  printf("\e[3m./archiver archive-name -e file.ext\e[0m\n\tExtrai um arquivo do archive sem remove-lo.\n");
+  printf("\e[3m./archiver archive-name -e file1.ext file2.ext...\e[0m\n\tExtrai varios arquivos do archive sem remove-lo.\n");
+  printf("\n\e[1mRemocao de arquivos:\e[0m\n");
+  printf("\e[3m./archiver archive-name -r file.ext\e[0m\n\tRemove um arquivo do archive.\n");
+  printf("\e[3m./archiver archive-name -r file1.ext file2.ext...\e[0m\n\tRemove varios arquivos do archive.\n\n");
+}
 
-  //Create teste.arc
-  create("teste");
-
-  //Add texto.txt to teste.arc
-  add("teste", "chart.png");
-  add("teste", "texto.txt");
-  printf("Arquivo Lido\n\n");
-  list("teste");
-  //delete("teste", "new.png");
+int main(int argc, char *argv[]) {
+  if(argc < 3) {
+    help();
+    return 0;
+  }
+  switch(argv[2][1]) {
+    case 'c':
+      create(argv[1]);
+      for (int i = 3; i < argc; ++i)
+        add(argv[1], argv[i]);
+      break;
+    case 'a':
+      for (int i = 3; i < argc; ++i)
+        add(argv[1], argv[i]);
+      break;
+    case 'l':
+      list(argv[1]);
+      break;
+    case 'e':
+      for (int i = 3; i < argc; ++i)
+        extract(argv[1], argv[i]);
+      break;
+    case 'r':
+      for (int i = 3; i < argc; ++i)
+        subtract(argv[1], argv[i]);
+      break;
+    default:
+      help();
+      break;
+  } return 0;
 }
